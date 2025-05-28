81c3c0c7762ba05d5613810558ee134dd4b7eb83
Marvin Winkler <m@rvin.at>
2025-05-28 16:45:42 +0200

    moved to debian

--- get-version.sh
+++ get-version.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 export IMG=$(docker build -q --pull --no-cache -t 'get-version' .)
-export TVHEADEND_VERSION=$(docker run --rm -t get-version apk list 2>/dev/null | grep '\[installed\]' | grep "^tvheadend-[0-9]" | cut -d " " -f1 | sed 's/tvheadend-//g' | tr -d '\r')
-export ALPINE_VERSION=$(docker run --rm -t get-version cat /etc/alpine-release | tail -n1 | tr -d '\r')
-[ -z "$ALPINE_VERSION" ] && exit 1
 
-export IMGTAG=$(echo "$1a$ALPINE_VERSION-t$TVHEADEND_VERSION")
+export DEBIAN_VERSION=$(docker run --rm -t get-version cat /etc/debian_version | tail -n1 | tr -d '\r')
+export TVHEADEND_VERSION=$(docker run --rm -t get-version dpkg --list tvheadend | grep '^ii' | sed 's/^[^0-9]*//g' | cut -d ' ' -f1 | sed 's/[+=]/_/g' | tr -d '\r')
+[ -z "$DEBIAN_VERSION" ] && exit 1
+
+export IMGTAG=$(echo "$1d$DEBIAN_VERSION-t$TVHEADEND_VERSION")
 export IMAGE_EXISTS=$(docker pull "$IMGTAG" 2>/dev/null >/dev/null; echo $?)
 
 # return latest, if container is already available :)
